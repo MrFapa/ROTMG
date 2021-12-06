038e7c467395c82b31438e685dba3e9d2735332c
MrFapa <s.scholzfabian@gmail.com>
2021-12-06 20:21:31 +0100

    v0.0.2
    Sprites for Dungeon, Rotating Projectiles

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -8,34 +8,38 @@ public class PlayerAttack : MonoBehaviour
 
     float attackSpeed;
     float nextTimeToShot = 0;
-    
-    void Start(){
+
+    void Start()
+    {
         attackSpeed = this.GetComponent<PlayerStats>().AttackSpeed;
     }
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
             shot();
     }
 
-    void shot(){
-        if (Time.time >= nextTimeToShot){ // Zeit bis zum nächsten Schuss
+    void shot()
+    {
+        if (Time.time >= nextTimeToShot)
+        { // Zeit bis zum nächsten Schuss
 
             float firerate = (weapon.Firerate * attackSpeed);
             nextTimeToShot = Time.time + 1f / firerate;
 
-            // neuer Schuss
-            GameObject bulletGameObject = Instantiate(weapon.Bullet);
-            Projectile bullet = bulletGameObject.GetComponent<Projectile>();
-
-            // Richung Maus
+            // Richung Maus und Rotation
             Vector3 mousePos = Camera.main.ScreenToWorldPoint((Input.mousePosition));
             mousePos.z = 0;     // z = 0 sonst verschwindet
             Vector3 dir = (mousePos - this.transform.position).normalized;
-            
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+            // neuer Schuss
+            GameObject bulletGameObject = Instantiate(weapon.Bullet, this.transform.position, Quaternion.Euler(new Vector3(0, 0, angle)));
+            Projectile bullet = bulletGameObject.GetComponent<Projectile>();
+
+
             // Stats der Bullet, abhängig von Weapon
             bullet.setDirection(dir);
-            bullet.updatePosition(this.transform.position);
             bullet.setSpeed(weapon.Speed);
             bullet.setRange(weapon.Range);
         }
